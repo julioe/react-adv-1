72314845367b6c9b12a1051a0ab1cfa6942e306f
Don Julio <julioejurado@gmail.com>
2022-11-21 10:24:42 -0600

    Control props alternativo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -3,31 +3,26 @@ import { products } from "../components/data/products";
 import { Product, ProductInCart } from "../interfaces/interfaces";
 
 
-
 export const useShoppingCart = (  ) => { 
 
  const [shoppingCart, setShoppingCart] = useState<{[key:string]: ProductInCart}>({ });  //el objeto ayuda para hacer un barrido mas facilmente
  // let valoresNuevos = Object.entries(shoppingCart);
   
  const onProductCountChange = ({ count, product}: {count: number, product: Product }) => {
-   setShoppingCart( oldShopingCart => {
-    const productInCart: ProductInCart = oldShopingCart[product.id] || { ...product, count: 0 };
-      
-      if( Math.max( productInCart.count + count, 0) > 0 ) {
-        productInCart.count += count;
-        return {
-          ...oldShopingCart,
-          [product.id]: productInCart
-        }
-      } 
-      // Borrar el producto
-      const { [product.id]: toDelate, ...rest } = oldShopingCart;
-    return {  ...rest  }
-    
+   setShoppingCart( oldShoppingCart => {
+    if( count === 0 ) {
+      const {  [product.id]: toDelete, ...rest  } = oldShoppingCart;
+      return rest;
+  }
+
+  return {
+      ...oldShoppingCart,
+      [ product.id ]: { ...product, count }
+  }
     })
   }
         return {
-              products,
+            
               shoppingCart,
 
               onProductCountChange
